1e7c11daedfbc52a6d013da6ac48a1ff7e1c1d92
MohammadHishamm <mohammad2109652@miuegypt.edu.eg>
2024-05-10 18:58:47 +0300

    login page

---
+++ program/program/Login.xaml.cs
@@ -0,0 +1,59 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace program
+{
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                Window.GetWindow(this).DragMove();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(passwordBox.Password) && passwordBox.Password.Length > 0)
+                textPassword.Visibility = Visibility.Collapsed;
+            else
+                textPassword.Visibility = Visibility.Visible;
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            passwordBox.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(passwordBox.Password))
+            {
+                MessageBox.Show("Successfully Signed In");
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+                textEmail.Visibility = Visibility.Collapsed;
+            else
+                textEmail.Visibility = Visibility.Visible;
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+    }
+}
--- program/program/MainWindow.xaml.cs
+++ program/program/MainWindow.xaml.cs
@@ -1,24 +1,22 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using program;
 
 namespace program
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // Navigate to the login page when the MainWindow is loaded
+            NavigateToLoginPage();
+        }
+
+        // Method to navigate to the login page
+        private void NavigateToLoginPage()
+        {
+            MainFrame.Navigate(new Login());
         }
     }
 }
